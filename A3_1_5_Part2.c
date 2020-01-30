e45eda8b536360934f5a4b64758e15bcb67298d3
allingtonc <60439030+allingtonc@users.noreply.github.com>
2020-01-30 08:52:43 -0500

    Add files via upload

---
+++ A3_1_5_Part2.c
@@ -0,0 +1,32 @@
+
+#pragma config(Sensor, in1,    lineFollower,   sensorLineFollower)
+#pragma config(Sensor, in2,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, in3,    lightSensor,    sensorReflection)
+#pragma config(Sensor, dgtl1,  limitSwitch,    sensorTouch)
+#pragma config(Sensor, dgtl2,  bumpSwitch,     sensorTouch)
+#pragma config(Sensor, dgtl3,  quad,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  sonar,          sensorSONAR_inch)
+#pragma config(Sensor, dgtl12, green,          sensorLEDtoVCC)
+#pragma config(Motor,  port1,           flashlight,    tmotorVexFlashlight, openLoop, reversed)
+#pragma config(Motor,  port2,           rightMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           servoMotor,    tmotorServoStandard, openLoop)
+// caleb
+//3.1.5 part2
+
+task main()
+{
+
+	int biggest;
+	while (1==1)
+	{
+	    biggest = 0;
+	    while (SensorValue(bumpSwitch)==0)
+	    {
+	        if (SensorValue(potentiometer   )>biggest)
+	        {
+	        biggest=SensorValue(potentiometer);
+	        }
+	    }
+	}
+}
